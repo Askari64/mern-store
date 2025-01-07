a72e65caf84bbeb64b9645ad5b33e2c0738d1774
Askari <askarir64@gmail.com>
2025-01-07 12:52:57 +0530

    DB file enhanced

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,36 @@
 import mongoose from "mongoose";
 
+let connectedState = false;
+
 export const connectDatabase = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${connect.connection.host}`)
+  try {
+    if (!process.env.MONGO_URI) {
+      console.log("🔴 MongoDB URI is not initialised");
+      process.exit(1);
     }
-    catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
+
+    if (connectedState) {
+      console.log("🟢 MongoDB is already connected");
     }
-}
+
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`🟢 MongoDB Connected: ${connect.connection.host}`);
+    connectedState = true;
+  } catch (error) {
+    console.log(`🔴 Error: ${error.message}`);
+    process.exit(1);
+  }
+
+  mongoose.connection.on("disconnectied", () => {
+    console.log("🔴 MongoDB has been disconnected");
+    connectedState = false;
+  });
+
+  process.on("SIGINT", async () => {
+    if (connectedState) {
+      await mongoose.connection.close();
+      console.log("🟡 MongoDB Connection has closed due to app termination");
+      process.exit(0);
+    }
+  });
+};
