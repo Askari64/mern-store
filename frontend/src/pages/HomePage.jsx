78b3da49b766e6224cf5fda6fa1f39c4274511c9
Askari <askarir64@gmail.com>
2025-01-12 18:37:52 +0530

    Fetching Products
    Fetching Products off Product Store.
    
    Not rendering yet though

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
-import { Container, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../Store/ProductStore";
 
 function HomePage() {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
+  console.log(`Products: ${products}`)
+
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
@@ -14,6 +22,13 @@ function HomePage() {
         >
           Current Products
         </Text>
+
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 3 }}
+          spacing={10}
+          w={"full"}
+        >{/** Product Cards will be mapped here */}</SimpleGrid>
+
         <Text
           fontSize={"xl"}
           textAlign={"center"}
