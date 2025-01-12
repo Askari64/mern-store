6ef0721387652909bbaabb913e61a54328e494d4
Askari <askarir64@gmail.com>
2025-01-13 02:10:05 +0530

    Conditional rendering
    Conditionally rendering no product found create product

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,9 +7,9 @@ import ProductCard from "../Components/ProductCard";
 function HomePage() {
   const { fetchProducts, products } = useProductStore();
   useEffect(() => {
-    fetchProducts()
-  }, [fetchProducts])
-  console.log(`Products: ${products}`)
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log(`Products: ${products}`);
 
   return (
     <Container maxW="container.xl" py={12}>
@@ -24,29 +24,36 @@ function HomePage() {
           Current Products
         </Text>
 
-        <SimpleGrid
-          columns={{ base: 1, md: 2, lg: 3 }}
-          spacing={10}
-          w={"full"}
-        >{products.map((product) => <ProductCard key={product._id} productImgURL={product.image} productName={product.name} productPrice={product.price}/>)}</SimpleGrid>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}>
+          {products.map((product) => (
+            <ProductCard
+              key={product._id}
+              productImgURL={product.image}
+              productName={product.name}
+              productPrice={product.price}
+            />
+          ))}
+        </SimpleGrid>
 
-        <Text
-          fontSize={"xl"}
-          textAlign={"center"}
-          fontWeight={"bold"}
-          color={"gray.500"}
-        >
-          No Products Found{" "}
-          <Link to={"/create"}>
-            <Text
-              as="span"
-              color={"blue.500"}
-              _hover={{ textDecoration: "underline" }}
-            >
-              Create a Product
-            </Text>
-          </Link>
-        </Text>
+        {products.length === 0 && (
+          <Text
+            fontSize={"xl"}
+            textAlign={"center"}
+            fontWeight={"bold"}
+            color={"gray.500"}
+          >
+            No Products Found{" "}
+            <Link to={"/create"}>
+              <Text
+                as="span"
+                color={"blue.500"}
+                _hover={{ textDecoration: "underline" }}
+              >
+                Create a Product
+              </Text>
+            </Link>
+          </Text>
+        )}
       </VStack>
     </Container>
   );
