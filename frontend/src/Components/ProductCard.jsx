676a47e59a101a30346e546cfebbe60cb8115f3e
Askari <askarir64@gmail.com>
2025-01-13 04:44:19 +0530

    delete function integration
    Integrated a delete function from Product Store

--- frontend/src/Components/ProductCard.jsx
+++ frontend/src/Components/ProductCard.jsx
@@ -7,12 +7,38 @@ import {
   Stack,
   Button,
   Box,
+  useToast,
 } from "@chakra-ui/react";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
+import { useProductStore } from "../Store/ProductStore.js";
 
 // eslint-disable-next-line react/prop-types
-function ProductCard({ productImgURL, productName, productPrice }) {
+function ProductCard({ productImgURL, productName, productPrice, productID }) {
+  const { deleteProduct } = useProductStore();
+  const toast = useToast();
+
+  const handleDelete = async (id) => {
+    const { success, message } = await deleteProduct(id);
+    if (!success === true) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <Card
       maxW={"sm"}
@@ -38,7 +64,12 @@ function ProductCard({ productImgURL, productName, productPrice }) {
             <Button size={"sm"} variant={"solid"} colorScheme="blue">
               <FaRegEdit />
             </Button>
-            <Button size={"sm"} variant={"solid"} colorScheme="red">
+            <Button
+              size={"sm"}
+              variant={"solid"}
+              colorScheme="red"
+              onClick={() => handleDelete(productID)}
+            >
               <MdDeleteOutline />
             </Button>
           </Box>
